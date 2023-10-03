b84418d35cefd17546b22e049a47494e472deaa1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-04 03:28:11 +0600

    request doc updated

--- src/Commands/RequestMakeCommand.php
+++ src/Commands/RequestMakeCommand.php
@@ -93,6 +93,7 @@ class RequestMakeCommand extends GeneratorCommand
             return <<<'HTML'
 'search' => ['string', 'nullable', 'max:255'],
             'per_page' => ['integer', 'nullable', 'min:1', 'max:255'],
+            'page' => ['integer', 'nullable', 'min:1'],
             'paginate' => ['boolean'],
             'sort' => ['string', 'nullable', 'min:2', 'max:255'],
             'dir' => ['string', 'min:3', 'max:4'],
